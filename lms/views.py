7041aafc07da2d3c1828fbe4f8d860c34c93cddd
bogdanovdenis95 <156674464+bogdanovdenis95@users.noreply.github.com>
2024-08-13 13:51:53 +0300

    25.1 upd

--- lms/views.py
+++ lms/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import IsAuthenticated
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
 from users.permissions import IsModerator, CanViewAndEditOnly
-from rest_framework.exceptions import PermissionDenied
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
@@ -12,61 +11,45 @@ class CourseViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.request.user.groups.filter(name='Модераторы').exists():
             if self.action in ['list', 'retrieve', 'update', 'partial_update']:
-                return [IsAuthenticated(), CanViewAndEditOnly()]
-            elif self.action in ['create', 'destroy']:
                 return [IsAuthenticated(), IsModerator()]
+            elif self.action in ['create', 'destroy']:
+                return [IsAuthenticated(), CanViewAndEditOnly()]
         return [IsAuthenticated()]
 
     def perform_create(self, serializer):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            raise PermissionDenied("You are not allowed to create courses.")
         serializer.save(owner=self.request.user)
-
-    def destroy(self, request, *args, **kwargs):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            raise PermissionDenied("You are not allowed to delete courses.")
-        return super().destroy(request, *args, **kwargs)
 
     def get_queryset(self):
         if self.request.user.groups.filter(name='Модераторы').exists():
             return Course.objects.all()
         return Course.objects.filter(owner=self.request.user)
 
-class LessonListCreateView(generics.ListCreateAPIView):
+class LessonViewSet(viewsets.ModelViewSet):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
     def get_permissions(self):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            if self.request.method == 'POST':
+        if self.action in ['list', 'retrieve', 'update', 'partial_update']:
+            if self.request.user.groups.filter(name='Модераторы').exists():
                 return [IsAuthenticated(), IsModerator()]
+            else:
+                return [IsAuthenticated()]
+        elif self.action == 'create':
+            if self.request.user.groups.filter(name='Модераторы').exists():
+                return [IsAuthenticated(), CanViewAndEditOnly()]
+            else:
+                return [IsAuthenticated()]
+        elif self.action == 'destroy':
+            if self.request.user.groups.filter(name='Модераторы').exists():
+                return [IsAuthenticated(), CanViewAndEditOnly()]
+            else:
+                return [IsAuthenticated()]
         return [IsAuthenticated()]
 
     def perform_create(self, serializer):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            raise PermissionDenied("You are not allowed to create lessons.")
         serializer.save(owner=self.request.user)
-
-
-class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Lesson.objects.all()
-    serializer_class = LessonSerializer
-
-    def get_permissions(self):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            if self.request.method in ['GET', 'PUT', 'PATCH']:
-                return [IsAuthenticated(), CanViewAndEditOnly()]
-            elif self.request.method == 'DELETE':
-                return [IsAuthenticated(), IsModerator()]
-        return [IsAuthenticated()]
-
-    def perform_destroy(self, instance):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            raise PermissionDenied("You are not allowed to delete lessons.")
-        return super().perform_destroy(instance)
 
     def get_queryset(self):
         if self.request.user.groups.filter(name='Модераторы').exists():
             return Lesson.objects.all()
         return Lesson.objects.filter(owner=self.request.user)
-
