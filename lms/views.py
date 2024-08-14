2408acae8255784c9d79a409a908096adbcd071f
bogdanovdenis95 <156674464+bogdanovdenis95@users.noreply.github.com>
2024-08-14 13:12:19 +0300

    25.1 upd

--- lms/views.py
+++ lms/views.py
@@ -2,18 +2,25 @@ from rest_framework import generics, viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Course, Lesson
 from .serializers import CourseSerializer, LessonSerializer
-from users.permissions import IsModerator, CanViewAndEditOnly
+from users.permissions import IsModerator, CanViewAndEditOnly, IsOwner, NotModerator
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
 
     def get_permissions(self):
-        if self.request.user.groups.filter(name='Модераторы').exists():
-            if self.action in ['list', 'retrieve', 'update', 'partial_update']:
+        if self.action in ['list', 'retrieve', 'update', 'partial_update']:
+            if self.request.user.groups.filter(name='Модераторы').exists():
                 return [IsAuthenticated(), IsModerator()]
-            elif self.action in ['create', 'destroy']:
-                return [IsAuthenticated(), CanViewAndEditOnly()]
+            else:
+                return [IsAuthenticated(), IsOwner()]
+        elif self.action == 'create':
+            return [IsAuthenticated(), NotModerator()]
+        elif self.action == 'destroy':
+            if self.request.user.groups.filter(name='Модераторы').exists():
+                return [IsAuthenticated(), NotModerator()]
+            else:
+                return [IsAuthenticated(), IsOwner()]
         return [IsAuthenticated()]
 
     def perform_create(self, serializer):
@@ -24,31 +31,38 @@ class CourseViewSet(viewsets.ModelViewSet):
             return Course.objects.all()
         return Course.objects.filter(owner=self.request.user)
 
-class LessonViewSet(viewsets.ModelViewSet):
+class LessonListCreateView(generics.ListCreateAPIView):
     queryset = Lesson.objects.all()
     serializer_class = LessonSerializer
 
     def get_permissions(self):
-        if self.action in ['list', 'retrieve', 'update', 'partial_update']:
-            if self.request.user.groups.filter(name='Модераторы').exists():
-                return [IsAuthenticated(), IsModerator()]
-            else:
-                return [IsAuthenticated()]
-        elif self.action == 'create':
-            if self.request.user.groups.filter(name='Модераторы').exists():
-                return [IsAuthenticated(), CanViewAndEditOnly()]
-            else:
-                return [IsAuthenticated()]
-        elif self.action == 'destroy':
-            if self.request.user.groups.filter(name='Модераторы').exists():
-                return [IsAuthenticated(), CanViewAndEditOnly()]
-            else:
-                return [IsAuthenticated()]
+        if self.request.method == 'POST':
+            return [IsAuthenticated(), NotModerator()]
         return [IsAuthenticated()]
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
+    def get_queryset(self):
+        if self.request.user.groups.filter(name='Модераторы').exists():
+            return Lesson.objects.all()
+        return Lesson.objects.filter(owner=self.request.user)
+
+class LessonRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+    def get_permissions(self):
+        if self.request.method in ['GET', 'PUT', 'PATCH']:
+            if self.request.user.groups.filter(name='Модераторы').exists():
+                return [IsAuthenticated(), IsModerator()]
+            return [IsAuthenticated(), IsOwner()]
+        elif self.request.method == 'DELETE':
+            if self.request.user.groups.filter(name='Модераторы').exists():
+                return [IsAuthenticated(), NotModerator()]
+            return [IsAuthenticated(), IsOwner()]
+        return [IsAuthenticated()]
+    
     def get_queryset(self):
         if self.request.user.groups.filter(name='Модераторы').exists():
             return Lesson.objects.all()
